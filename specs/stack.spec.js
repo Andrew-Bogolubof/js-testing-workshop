976923bd09b6f558e0a6ebea0013bc925e2cbb7e
Andrew-Bogolubof <a.bogolubof@gmail.com>
2019-05-24 19:43:00 +0300

    Initial preparation for task

---
+++ specs/stack.spec.js
@@ -0,0 +1,13 @@
+var Stack = require('../src/stack');
+
+describe('stack', function() {
+    describe('pop', function() {
+        it('should return last element of stack', function() {
+            var stack = new Stack();
+            //Execution
+
+            //
+            expect(result)
+        })
+    });
+});
---
+++ src/stack.js
@@ -0,0 +1,22 @@
+class Stack {
+    constructor(size) {
+
+    }
+    static merge() {
+
+    }
+    
+    push() {
+
+    }
+
+    pop() {
+
+    }
+
+    top() {
+
+    }
+}
+
+module.exports = Stack;
